97d6e07771f37bc8e09c4efd28769a747b90210e
Yosef Berman <yosefberman21@gmail.com>
2023-08-26 16:54:48 +0300

    layout midway

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -5,7 +5,7 @@ function coverCanvasWithImg(elImg) {
 
 function resizeCanvas() {                                          // Changing the canvas dimension clears the canvas
     const elContainer = document.querySelector('.canvas-container')
-    gElCanvas.width = elContainer.clientWidth
+    gElCanvas.width = elContainer.clientWidth-2
     // gElCanvas.height = elContainer.clientHeight
     renderMeme()
 }
